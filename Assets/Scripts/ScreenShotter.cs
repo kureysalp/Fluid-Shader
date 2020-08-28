0488601f0ce3efbf6aab977d8fafe63e24106e5e
Alp Kureyş Kilic <kureysalp@gmail.com>
2020-08-29 02:41:31 +0300

    Finalized shader

---
+++ Assets/Scripts/ScreenShotter.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenShotter : MonoBehaviour
+{
+    public bool captureScreenshot;
+    public string shotName;
+    int takeNumber;
+
+
+    IEnumerator RecordFrame()
+    {
+        captureScreenshot = false;
+        yield return new WaitForEndOfFrame();
+        var texture = ScreenCapture.CaptureScreenshotAsTexture();
+        // do something with texture
+        byte[] _byteArray = texture.EncodeToJPG();
+        System.IO.File.WriteAllBytes(Application.dataPath + $"/Screenshoot_{shotName}_{takeNumber}.png", _byteArray);
+
+        // cleanup
+        takeNumber++;
+        Object.Destroy(texture);
+    }
+
+    public void LateUpdate()
+    {
+        if (captureScreenshot)        
+            StartCoroutine(RecordFrame());
+                    
+    }
+}
